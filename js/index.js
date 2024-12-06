44994a02a70a287d2448bf12ab44cb3693cc1d76
AndresRamirez917 <andresmintic2021@gmail.com>
2024-12-06 17:31:54 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const btn_validar = document.getElementById('btn-validar').onclick = (e) => {
+    e.preventDefault();
+    const inputArr = [
+        {id: "nombre", value: "Nombre"},
+        {id: "email", value: "Email"},
+        {id: "fecha", value: "Fecha"},
+        {id: "hora", value: "Hora"},
+        {id: "mensaje", value: "Mensaje"}
+    ]
+    for(let {id, value} of inputArr){
+        const elemento 
+    }
+}
